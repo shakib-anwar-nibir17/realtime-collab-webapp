2df764907d020cd324fd1cfd6061a2d65c5c9e29
shakib-anwar-nibir17 <nibirshakib@gmail.com>
2024-03-28 11:33:57 +0600

    sidebar functinality

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,5 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
 const Navbar = () => {
-  return <div className="flex items-center gap-x-4 p-5 bg-green-600"></div>;
+  return (
+    <div className="flex items-center gap-x-4 p-5 bg-green-600">
+      <div className="hidden lg:flex lg:flex-1 bg-yellow-300">search</div>
+      <UserButton />
+    </div>
+  );
 };
 
 export default Navbar;
