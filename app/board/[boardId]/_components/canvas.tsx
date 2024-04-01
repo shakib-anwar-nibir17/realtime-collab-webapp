55fbfc36fdbce6647ee68d5bf6c13331fd40d80c
shakib-anwar-nibir17 <nibirshakib@gmail.com>
2024-04-01 19:34:47 +0600

    error fixing

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,23 +1,39 @@
 "use client";
 
+import { useState } from "react";
+
+import { useCanRedo, useCanUndo, useHistory } from "@/liveblocks.config";
+import { CanvasMode, CanvasState } from "@/types/canvas";
+
 import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
-
-import { useSelf } from "@/liveblocks.config";
 
 interface CanvasProps {
   boardId: string;
 }
 
 export const Canvas = ({ boardId }: CanvasProps) => {
-  const info = useSelf((me) => me.info);
-  console.log(info);
+  const [canvasState, setCanvasState] = useState<CanvasState>({
+    mode: CanvasMode.None,
+  });
+
+  const history = useHistory();
+  const canUndo = useCanUndo();
+  const canRedo = useCanRedo();
+
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
-      <Info />
+      <Info boardId={boardId} />
       <Participants />
-      <Toolbar />
+      <Toolbar
+        canvasState={canvasState}
+        setCanvasState={setCanvasState}
+        canRedo={canRedo}
+        canUndo={canUndo}
+        undo={history.undo}
+        redo={history.redo}
+      />
     </main>
   );
 };
---
+++ app/board/[boardId]/layout.tsx
@@ -0,0 +1,21 @@
+import { Toaster } from "@/components/ui/sonner";
+import { ConvexClientProvider } from "@/providers/convex-client-provider";
+import { ModalProvider } from "@/providers/modal-provider";
+
+interface DashboardLayoutProps {
+  children: React.ReactNode;
+}
+
+const BoardLayout = ({ children }: DashboardLayoutProps) => {
+  return (
+    <ConvexClientProvider>
+      <main className="h-full">
+        <ModalProvider />
+        {children}
+        <Toaster />
+      </main>
+    </ConvexClientProvider>
+  );
+};
+
+export default BoardLayout;
